0b36519eb722ca04d8e9e5e84b2242ff5a5a0c81
Meet5555 <bhimanimeet55555@gmail.com>
2024-02-26 21:03:45 +0530

    added search and pagination functionality

--- src/components/pages/Orders.jsx
+++ src/components/pages/Orders.jsx
@@ -1,14 +1,18 @@
 import React, { useEffect, useState } from "react"
-import { Link } from "react-router-dom"
-import {useDispatch, useSelector} from "react-redux"
-import {deleteOrder} from "../../redux/actions/actions"
+import { useDispatch, useSelector } from "react-redux"
+import { deleteOrder } from "../../redux/actions/actions"
 import Table from "../common/Table"
+import { TablePagination } from "@mui/material"
+import useDebounce from "../../CustomHooks/useDebounce"
 
 const Orders = () => {
-  const dispatch = useDispatch()
-  const orders = useSelector((state) => state.orderedBurgers)
+	const dispatch = useDispatch()
+	const orders = useSelector((state) => state.orderedBurgers)
 	const [ordersObj, setOrdersObj] = useState([])
-	const [searchQuery,setSearchQuery] = useState('');
+	const [searchQuery, setSearchQuery] = useState("")
+	const [page, setPage] = useState(0)
+	const [limit, setLimit] = useState(5)
+	const [tableData, setTableData] = useState([])
 
 	const fetchOrders = () => {
 		setOrdersObj(orders)
@@ -21,26 +25,84 @@ const Orders = () => {
 	const handleDeleteOrder = (orderId) => {
 		if (confirm(`Are you sure you want to cancel order ${orderId}`)) {
 			const updatedOrders = ordersObj.filter((order) => order.id !== orderId)
-			dispatch(deleteOrder(orderId));
+			dispatch(deleteOrder(orderId))
 			setOrdersObj(updatedOrders)
 		}
 	}
 
 	const search = (data) => {
-		return data.filter((item) => item.id.toString().includes(searchQuery.toString()));
+		// console.log("searching",searchQuery)
+		return data.filter((item) =>
+			item.id.toString().includes(searchQuery.toString())
+		)
 	}
+
+	const dataToShow = (data) => {
+		return data.slice(page * limit, (page + 1) * limit)
+	}
+
+	const handleChangeRowsPerPage = (event) => {
+		if (event.target.value == -1) {
+			setLimit(ordersObj.length)
+			setPage(0)
+		} else {
+			setLimit(parseInt(event.target.value, 10))
+			setPage(0)
+		}
+	}
+
+	const handleChangePage = (e, newPage) => {
+		setPage(newPage)
+	}
+
+	const debouncedQuery = useDebounce(searchQuery, 300)
+
+	useEffect(() => {
+		if (searchQuery !== "") {
+			setTableData(dataToShow(search(ordersObj)))
+		} else {
+			setTableData(dataToShow(ordersObj))
+		}
+	}, [debouncedQuery, page, limit, ordersObj])
 
 	return (
 		<div className="w-3/4 m-auto mt-5 bg-lime-100 dark:bg-gray-700 p-5">
-			<h1 className="text-center text-2xl font-semibold text-black dark:text-white">Your Orders</h1>
+			<h1 className="text-center text-2xl font-semibold text-black dark:text-white">
+				Your Orders
+			</h1>
 			<div className="search w-4/5 mt-5 mx-auto">
-				<input type="text" name="search" id="search" className="px-3 w-[100%] py-1 outline-none rounded bg-white dark:bg-gray-500 text-black dark:text-white focus:ring focus:border-blue-500" value={searchQuery} onChange={(e)=>setSearchQuery(e.target.value)} placeholder="search by id"/>
+				<input
+					type="text"
+					name="search"
+					id="search"
+					className="px-3 w-[100%] py-1 outline-none rounded bg-white dark:bg-gray-500 text-black dark:text-white focus:ring focus:border-blue-500"
+					value={searchQuery}
+					onChange={(e) => {
+						setPage(0)
+						setSearchQuery(e.target.value)
+					}}
+					placeholder="Search order by order id"
+				/>
 			</div>
 			<div className="order-container">
 				{ordersObj.length === 0 ? (
 					<h1>No orders</h1>
 				) : (
-					<Table ordersObj={search(ordersObj)} handleDeleteOrder={handleDeleteOrder} />
+					<>
+						<Table
+							ordersObj={tableData}
+							handleDeleteOrder={handleDeleteOrder}
+						/>
+						<TablePagination
+							component="div"
+							count={ordersObj.length}
+							page={page}
+							onPageChange={handleChangePage}
+							rowsPerPage={limit}
+							rowsPerPageOptions={[{ label: "All", value: -1 }, 5, 10, 25]}
+							onRowsPerPageChange={handleChangeRowsPerPage}
+						/>
+					</>
 				)}
 			</div>
 		</div>
