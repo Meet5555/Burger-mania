82ffead926eaa8880dc65ae1d60389cd7fcc4820
Meet5555 <bhimanimeet55555@gmail.com>
2024-02-27 11:05:27 +0530

    some optimizations

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -3,7 +3,7 @@ import {NavLink} from 'react-router-dom'
 
 const Home = () => {
   return (
-    <div className='h-[87vh] flex justify-center items-center bg-white dark:bg-gray-900'>
+    <div className='min-h-[87vh] flex justify-center items-center bg-white dark:bg-gray-900'>
       <button className='bg-teal-500 p-2 rounded-lg text-white'>
         <NavLink to='/recipes'>Order Burger</NavLink>
       </button>
